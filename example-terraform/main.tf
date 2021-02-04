b1744b1673d05a8ac984170e4863a74b9630b957
Ali <alikhajeh1@googlemail.com>
2021-02-04 15:59:44 +0000

    Increase IOPs

--- example-terraform/main.tf
+++ example-terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web_app" {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
     volume_size = 1000
-    iops        = 800
+    iops        = 900
   }
 }
 
